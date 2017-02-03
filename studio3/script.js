1aef8297d07ddf47eb01465d9618aacfb2966bc5
lnquan <lnquan@ucdavis.edu>
2017-02-02 18:36:31 -0800

    studio3 update

--- studio3/script.js
+++ studio3/script.js
@@ -5,14 +5,49 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
   var backpack =document.getElementById('backpack');
   var bigH = document.getElementById('bigH');
-  var bigPocket = document.getElementById('bigPocket');
 
+//when you click on the big bag, the contents open
   bigH.addEventListener('click', function(){
     bigPocket.style.display="block";
+    topPocket.style.display="none";
+    bottomPocket.style.display="none";
+    rightPocket.style.display="none";
+    leftPocket.style.display="none";
   });
 
-  backpack.addEventListener('click', function(){
-    bigPocket.style.display="none";
-  });
+  //when you click on the top pocket, the contents open
+    topH.addEventListener('click', function(){
+      topPocket.style.display="block";
+      bigPocket.style.display="none";
+      bottomPocket.style.display="none";
+      rightPocket.style.display="none";
+      leftPocket.style.display="none";
+    });
 
+    //when you click on the bottom pocket, the contents open
+      bottomH.addEventListener('click', function(){
+        bottomPocket.style.display="block";
+        topPocket.style.display="none";
+        bigPocket.style.display="none";
+        rightPocket.style.display="none";
+        leftPocket.style.display="none";
+      });
+
+      //when you click on the right pocket, the contents open
+        rightH.addEventListener('click', function(){
+          rightPocket.style.display="block";
+          bottomPocket.style.display="none";
+          topPocket.style.display="none";
+          bigPocket.style.display="none";
+          leftPocket.style.display="none";
+        });
+
+        //when you click on the left pocket, the contents open
+          leftH.addEventListener('click', function(){
+            leftPocket.style.display="block";
+            rightPocket.style.display="none";
+            bottomPocket.style.display="none";
+            topPocket.style.display="none";
+            bigPocket.style.display="none";
+          });
 });
