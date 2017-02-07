02557abeae0a91d656f466bbb28c787855c267c8
lnquan <lnquan@ucdavis.edu>
2017-02-07 02:35:37 -0800

    Studio 3 Update

--- studio3/script.js
+++ studio3/script.js
@@ -26,7 +26,6 @@ document.addEventListener("DOMContentLoaded", function(event) {
             bigp_h.style.display = "none";
         }
     }
-
     //when you click on the big bag, the contents open
     bigH.addEventListener('click', function() {
         hide();
@@ -35,39 +34,60 @@ document.addEventListener("DOMContentLoaded", function(event) {
             bigp_h.style.display = "block";
             bigp_h1.addEventListener('mouseover', function() {
                 bigp_h1.style.opacity = 1;
+                bigp_h1.addEventListener('click', function(){
+                  bigp_d1.style.display = "block";
+                })
             })
             bigp_h1.addEventListener('mouseout', function() {
                 bigp_h1.style.opacity = 0;
+                bigp_d1.style.display = "none";
             })
+
 
             bigp_h2.addEventListener('mouseover', function() {
                 bigp_h2.style.opacity = 1;
+                bigp_h2.addEventListener('click', function(){
+                  bigp_d2.style.display = "block";
+                })
             })
             bigp_h2.addEventListener('mouseout', function() {
                 bigp_h2.style.opacity = 0;
+                bigp_d2.style.display = "none";
             })
 
             bigp_h3.addEventListener('mouseover', function() {
                 bigp_h3.style.opacity = 1;
+                bigp_h3.addEventListener('click', function(){
+                  bigp_d3.style.display = "block";
+                })
             })
             bigp_h3.addEventListener('mouseout', function() {
                 bigp_h3.style.opacity = 0;
+                bigp_d3.style.display = "none";
             })
 
             bigp_h4.addEventListener('mouseover', function() {
                 bigp_h4.style.opacity = 1;
+                bigp_h4.addEventListener('click', function(){
+                  bigp_d4.style.display = "block";
+                })
             })
             bigp_h4.addEventListener('mouseout', function() {
                 bigp_h4.style.opacity = 0;
+                bigp_d4.style.display = "none";
             })
 
             bigp_h5.addEventListener('mouseover', function() {
                 bigp_h5.style.opacity = 1;
+                bigp_h5.addEventListener('click', function(){
+                  bigp_d5.style.display = "block";
+                })
             })
             bigp_h5.addEventListener('mouseout', function() {
                 bigp_h5.style.opacity = 0;
+                bigp_d5.style.display = "none";
             })
-        }
+          }
         topPocket.style.display = "none";
 
         bottomPocket.style.display = "none";
@@ -86,46 +106,72 @@ document.addEventListener("DOMContentLoaded", function(event) {
             topp_h.style.display = "block";
             tp_h1.addEventListener('mouseover', function() {
                 tp_h1.style.opacity = 1;
+                tp_h1.addEventListener('click', function(){
+                  tp_d1.style.display = "block";
+                })
             })
             tp_h1.addEventListener('mouseout', function() {
                 tp_h1.style.opacity = 0;
+                tp_d1.style.display = "none";
             })
 
             tp_h2.addEventListener('mouseover', function() {
                 tp_h2.style.opacity = 1;
+                tp_h2.addEventListener('click', function(){
+                  tp_d2.style.display = "block";
+            })
             })
             tp_h2.addEventListener('mouseout', function() {
                 tp_h2.style.opacity = 0;
+                tp_d2.style.display = "none";
             })
 
             tp_h3.addEventListener('mouseover', function() {
                 tp_h3.style.opacity = 1;
+                tp_h3.addEventListener('click', function(){
+                  tp_d3.style.display = "block";
+                })
             })
             tp_h3.addEventListener('mouseout', function() {
                 tp_h3.style.opacity = 0;
+                tp_d3.style.display = "none";
             })
 
             tp_h4.addEventListener('mouseover', function() {
                 tp_h4.style.opacity = 1;
+                tp_h4.addEventListener('click', function(){
+                  tp_d4.style.display = "block";
+            })
             })
             tp_h4.addEventListener('mouseout', function() {
                 tp_h4.style.opacity = 0;
+                tp_d4.style.display = "none";
             })
 
             tp_h5.addEventListener('mouseover', function() {
                 tp_h5.style.opacity = 1;
+                tp_h5.addEventListener('click', function(){
+                  tp_d5.style.display = "block";
             })
+            })
+
             tp_h5.addEventListener('mouseout', function() {
                 tp_h5.style.opacity = 0;
+                tp_d5.style.display = "none";
             })
+
 
             tp_h6.addEventListener('mouseover', function() {
                 tp_h6.style.opacity = 1;
+                tp_h6.addEventListener('click', function(){
+                  tp_d6.style.display = "block";
+            })
             })
             tp_h6.addEventListener('mouseout', function() {
                 tp_h6.style.opacity = 0;
+                tp_d6.style.display = "none";
             })
-        }
+            }
 
         bigPocket.style.display = "none";
         bottomPocket.style.display = "none";
@@ -142,21 +188,29 @@ document.addEventListener("DOMContentLoaded", function(event) {
             botp_h.style.display = "block";
             bp_h1.addEventListener('mouseover', function() {
                 bp_h1.style.opacity = 1;
+                bp_h1.addEventListener('click', function(){
+                  bp_d1.style.display = "block";
+                })
             })
             bp_h1.addEventListener('mouseout', function() {
                 bp_h1.style.opacity = 0;
+                bp_d1.style.display = "none";
             })
 
             bp_h2.addEventListener('mouseover', function() {
                 bp_h2.style.opacity = 1;
+                bp_h2.addEventListener('click', function(){
+                  bp_d2.style.display = "block";
+                })
             })
             bp_h2.addEventListener('mouseout', function() {
                 bp_h2.style.opacity = 0;
+                bp_d2.style.display = "none";
             })
         }
-        topPocket.style.display = "none";
         rightPocket.style.display = "none";
         leftPocket.style.display = "none";
+        topPocket.style.display = "none";
     });
 
     //when you click on the right pocket, the contents open
@@ -168,9 +222,13 @@ document.addEventListener("DOMContentLoaded", function(event) {
             rp_h.style.display = "block";
             rp_h1.addEventListener('mouseover', function() {
                 rp_h1.style.opacity = 1;
+                rp_h1.addEventListener('click', function(){
+                  rp_d1.style.display = "block";
+                })
             })
             rp_h1.addEventListener('mouseout', function() {
                 rp_h1.style.opacity = 0;
+                rp_d1.style.display="none";
             })
         }
         bottomPocket.style.display = "none";
@@ -190,18 +248,25 @@ document.addEventListener("DOMContentLoaded", function(event) {
             lp_h.style.display = "block";
             lp_h1.addEventListener('mouseover', function() {
                 lp_h1.style.opacity = 1;
+                lp_h1.addEventListener('click', function(){
+                  lp_d1.style.display = "block";
+                })
             })
             lp_h1.addEventListener('mouseout', function() {
                 lp_h1.style.opacity = 0;
+                lp_d1.style.display = "none";
             })
             lp_h2.addEventListener('mouseover', function() {
                 lp_h2.style.opacity = 1;
+                lp_h2.addEventListener('click', function(){
+                  lp_d2.style.display = "block";
+                })
             })
             lp_h2.addEventListener('mouseout', function() {
                 lp_h2.style.opacity = 0;
+                lp_d2.style.display = "none";
             })
         }
-
         rightPocket.style.display = "none";
         bottomPocket.style.display = "none";
         topPocket.style.display = "none";
@@ -209,7 +274,4 @@ document.addEventListener("DOMContentLoaded", function(event) {
         bigPocket.style.display = "none";
 
     });
-    // if bigPocket is displayed than let hover work for highlight the objects and selection
-
-
 });
