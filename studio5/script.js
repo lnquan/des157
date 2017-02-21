89c7d81d208ec2ba9e1a28e2fcc4c1a717bf70e0
lnquan <lnquan@ucdavis.edu>
2017-02-21 12:08:39 -0800

    update

--- studio5/script.js
+++ studio5/script.js
@@ -5,16 +5,16 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
 
-var mymap = L.map('map').setView([38.543304, -121.754189], 19);
+    var mymap = L.map('map').setView([38.543304, -121.754189], 19);
 
-L.tileLayer('https://api.mapbox.com/styles/v1/lnquan/ciz90morr001k2ss20yjnm8mz/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoibG5xdWFuIiwiYSI6ImNpejkwbGxlbjAxYXYyd284d2EyMmt1djQifQ.n0tAM06NGvw7Q4SEEdbY4w', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    maxZoom: 50,
-    id: 'your.mapbox.project.id',
-    accessToken: 'pk.eyJ1IjoibG5xdWFuIiwiYSI6ImNpejkwbGxlbjAxYXYyd284d2EyMmt1djQifQ.n0tAM06NGvw7Q4SEEdbY4w'
-}).addTo(mymap);
+    L.tileLayer('https://api.mapbox.com/styles/v1/lnquan/ciz90morr001k2ss20yjnm8mz/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoibG5xdWFuIiwiYSI6ImNpejkwbGxlbjAxYXYyd284d2EyMmt1djQifQ.n0tAM06NGvw7Q4SEEdbY4w', {
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+        maxZoom: 50,
+        id: 'your.mapbox.project.id',
+        accessToken: 'pk.eyJ1IjoibG5xdWFuIiwiYSI6ImNpejkwbGxlbjAxYXYyd284d2EyMmt1djQifQ.n0tAM06NGvw7Q4SEEdbY4w'
+    }).addTo(mymap);
 
-L.control.geocoder('mapzen-hEmECJd').addTo(mymap);
+    L.control.geocoder('mapzen-hEmECJd').addTo(mymap);
 
-var marker = L.marker([38.543304, -121.754189]).addTo(mymap);
+    var marker = L.marker([38.543304, -121.754189]).addTo(mymap);
 });
