b988589c61dbcbf55bf628436b93a3448f7e5dbe
lnquan <lnquan@ucdavis.edu>
2017-03-09 12:59:34 -0800

    map update

--- project/proto2/script.js
+++ project/proto2/script.js
@@ -2,38 +2,54 @@
 
 document.addEventListener("DOMContentLoaded", function() {
 
+    console.log("DOM fully loaded and parsed");
+
+
   help.addEventListener("click", function(){
     confirm("Do you need help?")
   })
 
   locate.addEventListener("click", function(){
     confirm("Share your location to your friends?")
+
+    // if (confirm("Share your location to your friends?") == return true){
+    //
+    // }
   })
-    console.log("DOM fully loaded and parsed");
+//     var latitude = location.coords.latitude;
+//     var longitude = location.coords.longitude;
+//
+//     var getLocation = function() {
+//       if (typeof navigator !== "undefined" && typeof navigator.geolocation !== "undefined") {
+//         log("Asking user to get their location");
+//         navigator.geolocation.getCurrentPosition(geolocationCallback, errorHandler);
+//       } else {
+//         log("Your browser does not support the HTML5 Geolocation API, so this demo will not work.")
+//       }
+//     };
+//
+//     var mymap = L.map('map').setView([latitude, longitude], 19);
+// console.log(latitude, longitude);
+//     L.tileLayer('https://api.mapbox.com/styles/v1/lnquan/ciz90morr001k2ss20yjnm8mz/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoibG5xdWFuIiwiYSI6ImNpejkwbGxlbjAxYXYyd284d2EyMmt1djQifQ.n0tAM06NGvw7Q4SEEdbY4w', {
+//         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+//         maxZoom: 50,
+//         id: 'your.mapbox.project.id',
+//         accessToken: 'pk.eyJ1IjoibG5xdWFuIiwiYSI6ImNpejkwbGxlbjAxYXYyd284d2EyMmt1djQifQ.n0tAM06NGvw7Q4SEEdbY4w'
+//     }).addTo(mymap);
+//
+//     L.control.geocoder('mapzen-hEmECJd').addTo(mymap);
+//
+//     var marker = L.marker([latitude, longitude]).addTo(mymap);
+var mymap = L.map('map').setView([38.543304, -121.754189], 19);
 
-    var latitude = location.coords.latitude;
-    var longitude = location.coords.longitude;
+  L.tileLayer('https://api.mapbox.com/styles/v1/lnquan/ciz90morr001k2ss20yjnm8mz/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoibG5xdWFuIiwiYSI6ImNpejkwbGxlbjAxYXYyd284d2EyMmt1djQifQ.n0tAM06NGvw7Q4SEEdbY4w', {
+      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+      maxZoom: 50,
+      id: 'your.mapbox.project.id',
+      accessToken: 'pk.eyJ1IjoibG5xdWFuIiwiYSI6ImNpejkwbGxlbjAxYXYyd284d2EyMmt1djQifQ.n0tAM06NGvw7Q4SEEdbY4w'
+  }).addTo(mymap);
 
-    var getLocation = function() {
-      if (typeof navigator !== "undefined" && typeof navigator.geolocation !== "undefined") {
-        log("Asking user to get their location");
-        navigator.geolocation.getCurrentPosition(geolocationCallback, errorHandler);
-      } else {
-        log("Your browser does not support the HTML5 Geolocation API, so this demo will not work.")
-      }
-    };
+  L.control.geocoder('mapzen-hEmECJd').addTo(mymap);
 
-    var mymap = L.map('map').setView([latitude, longitude], 19);
-console.log(latitude, longitude);
-    L.tileLayer('https://api.mapbox.com/styles/v1/lnquan/ciz90morr001k2ss20yjnm8mz/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoibG5xdWFuIiwiYSI6ImNpejkwbGxlbjAxYXYyd284d2EyMmt1djQifQ.n0tAM06NGvw7Q4SEEdbY4w', {
-        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-        maxZoom: 50,
-        id: 'your.mapbox.project.id',
-        accessToken: 'pk.eyJ1IjoibG5xdWFuIiwiYSI6ImNpejkwbGxlbjAxYXYyd284d2EyMmt1djQifQ.n0tAM06NGvw7Q4SEEdbY4w'
-    }).addTo(mymap);
-
-    L.control.geocoder('mapzen-hEmECJd').addTo(mymap);
-
-    var marker = L.marker([latitude, longitude]).addTo(mymap);
-
+  var marker = L.marker([38.543304, -121.754189]).addTo(mymap);
 });
