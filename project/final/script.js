92f5d945417deed551aba8b49651e79b6a3e901b
lnquan <lnquan@ucdavis.edu>
2017-03-23 22:19:34 -0700

    geolocate update
    + other icons and buttons

--- project/final/script.js
+++ project/final/script.js
@@ -2,30 +2,34 @@
 
 document.addEventListener("DOMContentLoaded", function() {
     console.log("DOM fully loaded and parsed");
-    var lat;
-    var lng;
 
-    function getLocation() {
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(getCoordinates);
-        } else {
-            alert("4");
-        }
-    }
-
-    function getCoordinates(position) {
-        lat = position.coords.latitude;
-        lng = position.coords.longitude;
-
-        // now latitude and longitude are available
-
-    }
-    getLocation();
+    // alert("This is meant for mobile (specifically iPhone 6). Please open the console in Chrome to view in mobile.");
+//////////////////// This Code Allows Geolocation to Work///////////////////////
+    // var lat;
+    // var lng;
+    //
+    // function getLocation() {
+    //     if (navigator.geolocation) {
+    //         navigator.geolocation.getCurrentPosition(getCoordinates);
+    //     } else {
+    //         alert("4");
+    //     }
+    // }
+    //
+    // function getCoordinates(position) {
+    //     lat = position.coords.latitude;
+    //     lng = position.coords.longitude;
+    //
+    //     // now latitude and longitude are available
+    //
+    // }
+    // getLocation();
 
     mapboxgl.accessToken = 'pk.eyJ1IjoibG5xdWFuIiwiYSI6ImNpejkwbGxlbjAxYXYyd284d2EyMmt1djQifQ.n0tAM06NGvw7Q4SEEdbY4w';
 
     var map = new mapboxgl.Map({
         container: 'map',
+        ////////Centers to Current Location, but doesn't work with MapBox's JS ///////
         // center:[lat,lng],
         style: 'mapbox://styles/lnquan/cj031ahqh001v2rsb2qz83etk'
     });
