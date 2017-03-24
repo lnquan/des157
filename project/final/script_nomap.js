3325ca9e8fc4c1e81d261864d581ec9ccbaae22e
lnquan <lnquan@ucdavis.edu>
2017-03-24 02:03:30 -0700

    update with several changes

--- project/final/script_nomap.js
+++ project/final/script_nomap.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function() {
     console.log("DOM fully loaded and parsed");
 
     // alert("This is meant for mobile (specifically iPhone 6). Please open the console in Chrome to view in mobile.");
-////////////////// This Code Allows Geolocation to Work///////////////////////
+//////////////////// This Code Allows Geolocation to Work///////////////////////
     // var lat;
     // var lng;
     //
@@ -25,28 +25,28 @@ document.addEventListener("DOMContentLoaded", function() {
     // }
     // getLocation();
 
-    mapboxgl.accessToken = 'pk.eyJ1IjoibG5xdWFuIiwiYSI6ImNpejkwbGxlbjAxYXYyd284d2EyMmt1djQifQ.n0tAM06NGvw7Q4SEEdbY4w';
-
-    var map = new mapboxgl.Map({
-        container: 'map',
-        ////////Centers to Current Location, but doesn't work with MapBox's JS ///////
-        // center:[lat,lng],
-        style: 'mapbox://styles/lnquan/cj031ahqh001v2rsb2qz83etk'
-    });
-    console.log("var map testing");
-// Add geolocate control to the map.
-map.addControl(new mapboxgl.GeolocateControl({
-    positionOptions: {
-        enableHighAccuracy: true
-    }
-
-}));
-  console.log("location accuracy loaded");
-
-  map.addControl(new MapboxGeocoder({
-    accessToken: mapboxgl.accessToken
-
-}));
+//     mapboxgl.accessToken = 'pk.eyJ1IjoibG5xdWFuIiwiYSI6ImNpejkwbGxlbjAxYXYyd284d2EyMmt1djQifQ.n0tAM06NGvw7Q4SEEdbY4w';
+//
+//     var map = new mapboxgl.Map({
+//         container: 'map',
+//         ////////Centers to Current Location, but doesn't work with MapBox's JS ///////
+//         // center:[lat,lng],
+//         style: 'mapbox://styles/lnquan/cj031ahqh001v2rsb2qz83etk'
+//     });
+//     console.log("var map testing");
+// // Add geolocate control to the map.
+// map.addControl(new mapboxgl.GeolocateControl({
+//     positionOptions: {
+//         enableHighAccuracy: true
+//     }
+//
+// }));
+//   console.log("location accuracy loaded");
+//
+//   map.addControl(new MapboxGeocoder({
+//     accessToken: mapboxgl.accessToken
+//
+// }));
 
 var menuButton = document.getElementById('menuButton');
 
