addfa5419113c920ba55c3b9832ce31af6b2d20c
lnquan <lnquan@ucdavis.edu>
2017-03-24 12:44:52 -0700

    update (several pages upload)

--- project/final/script.js
+++ project/final/script.js
@@ -3,34 +3,33 @@
 document.addEventListener("DOMContentLoaded", function() {
     console.log("DOM fully loaded and parsed");
 
-    // alert("This is meant for mobile (specifically iPhone 6). Please open the console in Chrome to view in mobile.");
-////////////////// This Code Allows Geolocation to Work///////////////////////
-    // var lat;
-    // var lng;
-    //
-    // function getLocation() {
-    //     if (navigator.geolocation) {
-    //         navigator.geolocation.getCurrentPosition(getCoordinates);
-    //     } else {
-    //         alert("4");
-    //     }
-    // }
-    //
-    // function getCoordinates(position) {
-    //     lat = position.coords.latitude;
-    //     lng = position.coords.longitude;
-    //
-    //     // now latitude and longitude are available
-    //
-    // }
-    // getLocation();
+    alert("This is meant for mobile (specifically iPhone 6). Please open the console in Chrome to view in mobile.");
+//////////////// This Code Allows Geolocation to Work///////////////////////
+    var lat;
+    var lng;
+
+    function getLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(getCoordinates);
+        } else {
+            alert("4");
+        }
+    }
+
+    function getCoordinates(position) {
+        lat = position.coords.latitude;
+        lng = position.coords.longitude;
+
+        // now latitude and longitude are available
+
+    }
+    getLocation();
 
     mapboxgl.accessToken = 'pk.eyJ1IjoibG5xdWFuIiwiYSI6ImNpejkwbGxlbjAxYXYyd284d2EyMmt1djQifQ.n0tAM06NGvw7Q4SEEdbY4w';
 
     var map = new mapboxgl.Map({
         container: 'map',
         ////////Centers to Current Location, but doesn't work with MapBox's JS ///////
-        // center:[lat,lng],
         style: 'mapbox://styles/lnquan/cj031ahqh001v2rsb2qz83etk'
     });
     console.log("var map testing");
@@ -40,7 +39,9 @@ map.addControl(new mapboxgl.GeolocateControl({
         enableHighAccuracy: true
     }
 
+
 }));
+
   console.log("location accuracy loaded");
 
   map.addControl(new MapboxGeocoder({
